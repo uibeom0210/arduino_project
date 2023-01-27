b79b07aa159d5fa7c776e43c32e4da42c4c44408
uibeom0210 <wjddmlqja17@gmail.com>
2023-01-27 13:10:54 +0900

    Refactor functions from wifiHandler.cpp for HTTP
    1. HTTP setup
    	(1) Connect API server through AT command CIPSTART.
    	(2) Select CIPMODE=1 to use Passthrough Receiving Mode.
    	(3) Send AT+CIPSEND to enter Passthrough Mode.
    2. Get method
    	(1) Use www.data.go.kr API to get weather information response.
    
    3. Add getIPAdress function to get assigned IP adress.
    4. Split sendData function to sendAT and sendData function. Also remove
       parameter timeout in sendData function.

--- wifi_example/wifi_example.ino
+++ wifi_example/wifi_example.ino
@@ -4,7 +4,7 @@
 #define LED 13
 
 extern SoftwareSerial ESP8266_serial;
-String income_wifi = "";
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -18,25 +18,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (ESP8266_serial.available()) {
-    if (ESP8266_serial.find("+IPD")) {
-      income_wifi = ESP8266_serial.readStringUntil('\r');
-      String wifi_temp = income_wifi.substring(income_wifi.indexOf("GET /") + 5, income_wifi.indexOf("HTTP/1.1") - 1);
-      if (wifi_temp == "LED_ON") {
-        digitalWrite(LED, HIGH);
-        delay(500);
-      } else if (wifi_temp == "LED_OFF") {
-        digitalWrite(LED, LOW);
-        delay(500);
-      } else {
-        //
-      }
-      Serial.println(income_wifi);
-    }
-  } 
-  else {
-    delay(1000);
-  }
+  //getData();
 }
-
 
