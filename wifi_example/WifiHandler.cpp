46594424b766c316367618c03058227bd162e4c7
uibeom0210 <wjddmlqja17@gmail.com>
2023-01-27 13:10:54 +0900

    Refactor functions from wifiHandler.cpp for HTTP
    1. HTTP setup
    	(1) Connect API server through AT command CIPSTART.
    	(2) Select CIPMODE=1 to use Passthrough Receiving Mode.
    	(3) Send AT+CIPSEND to enter Passthrough Mode.
    2. Get method
    	(1) Use www.data.go.kr API to get weather information response.
    
    3. Add getIPAdress function to get assigned IP adress.
    4. Split sendData function to sendAT and sendData function. Also remove
       parameter timeout in sendData function.

--- wifi_example/WifiHandler.cpp
+++ wifi_example/WifiHandler.cpp
@@ -9,27 +9,73 @@
   */
 
 #include "WifiHandler.h"
+#define SERVER_IP "27.101.215.194"
+
 SoftwareSerial ESP8266_serial(WIFI_RXD, WIFI_TXD);
+String assignedIP = "";
+String income_wifi = "";
+
+// char* getWeatherData[6] = {"GET /",
+//                       "1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey",
+//                       "=vwa%2FDDlSjq1JMK4NX8ZJ983sV0p3pCZUSE%2FD3q6PIv193RHWhA35iY8",
+//                       "mKkmifefajFco03SsO2lCru6g0LPPYw%3D%3D&pageNo=1&numOfRows=10&d",
+//                       "ataType=JSON&base_date=20230127&base_time=1200&nx=58&ny=126\r\n"};
+
+String getWeatherData1 = "GET /1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=vwa%2FDDlSjq1JMK4NX8ZJ983sV0p3pCZUSE%2FD3q6PIv193RHWhA35iY8mKkmifefajFco03SsO2lCru6g0LPPYw%3D%3D&pageNo=1&numOfRows=10&dataType=JSON&base_date=20230127&base_time=1100&nx=58&ny=126\r\n";
 
 void wifiSetup(){
-	sendData("AT+RST\r\n", 2000);
-	sendData("AT+CWMODE=3\r\n", 1000);
-	sendData("AT+CWSAP=\"KCCI_STC_S\",\"kcci098#\",11,0\r\n", 1000);
-	sendData("AT+CWSAP?\r\n", 1000);
-
+	sendAT("AT+RST\r\n", 2000);
+	sendAT("AT+CWMODE=3\r\n", 1000);
+	sendAT("AT+CWSAP=\"KCCI_STC_S\",\"kcci098#\",11,0\r\n", 1000);
+	sendAT("AT+CWSAP?\r\n", 1000);
 	String income_CIFSR = "";
 	do {
-    income_CIFSR = sendData("AT+CIFSR\r\n", 5000);
+    income_CIFSR = sendAT("AT+CIFSR\r\n", 5000);
 	}while (income_CIFSR.indexOf("0.0.0.0") != -1);
-	sendData("AT+CIPMUX=1\r\n", 1000);
-	sendData("AT+CIPSERVER=1,80\r\n", 1000);
+
+  char cifsr[50];
+  assignedIP = getIpAdress(cifsr, income_CIFSR);
+
+  sendAT("AT+CIPSTART=\"TCP\",\"27.101.215.194\",80\r\n",1000);
+  sendAT("AT+CIPMODE=1\r\n",1000);
+  sendAT("AT+CIPMUX=0\r\n",1000);
+  sendAT("AT+CIPSEND\r\n",1000);
+  sendData(getWeatherData1);
+  
 	Serial.println("available");
 }
 
-String sendData(String command, const int timeout){
+String getIpAdress(char str[], String income){
+  unsigned int length = income.length();
+  income.toCharArray(str, length);
+  int i = 0;
+  char *sArr[5];
+  char * ptr = NULL;
+
+  ptr = strtok(str, "\r\n");
+  while(ptr != NULL){
+    sArr[i++] = ptr;
+    ptr = strtok(NULL, "\r\n");
+  }
+  Serial.println(sArr[2]);
+  return sArr[2];
+}
+
+void getData(){
+  if (ESP8266_serial.available()) {
+    if (ESP8266_serial.find("+IPD")) {
+      income_wifi = ESP8266_serial.readStringUntil('\r');
+    }
+    Serial.println(income_wifi);
+    //return income_wifi;
+  } 
+}
+
+String sendAT(String atCommand, const int timeout){
 	String response = "";
-	ESP8266_serial.print(command);
+	ESP8266_serial.print(atCommand);
 	long int time = millis();
+  
 	while ((time + timeout) > millis()) {
 		while (ESP8266_serial.available()) {
 			char c = ESP8266_serial.read();
@@ -39,3 +85,22 @@ String sendData(String command, const int timeout){
 	Serial.print(response);
 	return response;
 }
+
+void sendData(String command){
+	String response = "";
+	ESP8266_serial.print(command);
+  char c;
+	while(1){
+    while (ESP8266_serial.available()) {
+			c = ESP8266_serial.read();
+			response += c;
+		}
+    if(c == '\r'){
+      break;
+    }
+  }
+  Serial.println(response);
+	// Serial.print(response);
+	// return response;
+}
+
