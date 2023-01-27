d518c79b11d004f7eebde38ab357ab53d167ea25
uibeom0210 <wjddmlqja17@gmail.com>
2023-01-27 18:19:51 +0900

    update pirSensor

--- pirSensor/pirSensor.ino
+++ pirSensor/pirSensor.ino
@@ -1,33 +1,55 @@
-int led1 = 6; // green
-int led2 = 5;
-int sensor = 7;
-int priState = LOW;
-int val = 0;
+
+#include <avr/sleep.h>
+#include <avr/power.h>
+
+#define LED13 13
+#define LED1 6 // green
+#define LED2 5
+#define BUTTON 2
+
+#define SLEEP 0
+#define WAKE 1
+
+volatile byte sleepState = SLEEP;
+int pirValue = 0;
+
+ISR(WDT_vect){
+
+}
 
 void setup() {
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(sensor, INPUT);
+  pinMode(LED13, OUTPUT);
+  digitalWrite(LED13, LOW);
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  val = digitalRead(sensor);
-
-  if (val == LOW) {
-    digitalWrite(led1, HIGH);
-    digitalWrite(led2, LOW);
-    if (priState == LOW) {
-      Serial.println("HI");
-      priState = HIGH;
-    }
+  
+  if(sleepState == WAKE){
+    digitalWrite(LED13, HIGH);
+    delay(5000);
+    sleepState = SLEEP;
   }
-  else {
-    digitalWrite(led1, LOW);
-    digitalWrite(led2, HIGH);
-    if (priState == HIGH) {
-      Serial.println("Bye");
-      priState = LOW;
-    }
-  }
+  else if(sleepState == SLEEP){
+    sleep();
+  }  
 }
+
+void sleep(){
+  sleep_enable();
+  attachInterrupt(digitalPinToInterrupt(BUTTON), buttonClick, FALLING);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  digitalWrite(LED13, LOW);
+  delay(1000);
+  sleep_mode(); // CPU sleep
+  Serial.println("just woke up!");
+  detachInterrupt(digitalPinToInterrupt(BUTTON));
+}
+
+void buttonClick(){
+  sleepState = WAKE;
+  sleep_disable(); // When wake
+}
+
+
