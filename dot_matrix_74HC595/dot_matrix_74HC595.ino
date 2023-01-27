8382b1cc02e9d4d76a29aad848ea1d69ea211c32
jw2635 <jw2635@naver.com>
2023-01-27 18:05:37 +0900

    Simplify the dot matrix array

--- dot_matrix_74HC595/dot_matrix_74HC595.ino
+++ dot_matrix_74HC595/dot_matrix_74HC595.ino
@@ -1,6 +1,24 @@
 #include <MsTimer2.h>
 
 #define MATRIX_SIZE 8 // 8 * 8
+
+#define MATRIX_SUN { \
+  {0b00010000, 0b00000000, 0b00011000, 0b00111101, 0b10111100, 0b00011000, 0b00000000, 0b00001000}, \
+  {0b00000000, 0b01000010, 0b00011000, 0b00111100, 0b00111100, 0b00011000, 0b01000010, 0b00000000}, \
+}
+#define MATRIX_CLOUD { \
+  {0b01111000, 0b11111100, 0b01111000, 0b00000000, 0b00011110, 0b00111111, 0b00011110, 0b00000000}, \
+  {0b00011110, 0b00111111, 0b00011110, 0b00000000, 0b01111000, 0b11111100, 0b01111000, 0b00000000}, \
+}
+#define MATRIX_RAIN { \
+  {0b01000010, 0b01001010, 0b01001010, 0b00001000, 0b00100001, 0b10100101, 0b10100101, 0b10000100}, \
+  {0b10001010, 0b00000000, 0b01000010, 0b01001010, 0b01001010, 0b00001000, 0b00100001, 0b10100101}, \
+}
+#define MATRIX_SNOW { \
+  {0b01000001, 0b00000100, 0b00010000, 0b10000000, 0b00001001, 0b00100000, 0b00000000, 0b10000010}, \
+  {0b10001000, 0b00000000, 0b01000001, 0b00000100, 0b00010000, 0b10000000, 0b00001001, 0b00100000}, \
+}
+
 // Column Pin
 #define C1 A3
 #define C2 8
@@ -28,37 +46,16 @@
 
 // Bit extract macro function : data 에서 loc 번째 비트 부터 area 영역 만큼 추출
 // ex) extract_bits( 0b00000010, 1, 2) 결과 1 -> 0b00000010 데이터의 2번째 비트 1크기 만큼 추출  
-#define extract_bits(data, area, loc)   (((data) >> (loc)) & (area))
+#define extract_bits(data, area, loc) (((data) >> (loc)) & (area))
 
 uint8_t colPins[8]={ C1, C2, C3, C4, C5, C6, C7, C8};
 
-int sensor = 7;
-int priState = LOW;
-int val = 0;
-
-uint8_t matrixSun[2][8] ={{0b00010000, 0b00000000, 0b00011000, 0b00111101, 0b10111100, 0b00011000, 0b00000000, 0b00001000},
-                            {0b00000000, 0b01000010, 0b00011000, 0b00111100, 0b00111100, 0b00011000, 0b01000010, 0b00000000}};
-
-//uint8_t matrixCloud[2][8] ={{0b01111000, 0b11111100, 0b01111000, 0b00000000, 0b00011110, 0b00111111, 0b00011110, 0b00000000},
-//                            {0b00011110, 0b00111111, 0b00011110, 0b00000000, 0b01111000, 0b11111100, 0b01111000, 0b00000000}};
-
-//uint8_t matrixRain[2][8] ={{0b01000010, 0b01001010, 0b01001010, 0b00001000, 0b00100001, 0b10100101, 0b10100101, 0b10000100},
-//                            {0b10001010, 0b00000000, 0b01000010, 0b01001010, 0b01001010, 0b00001000, 0b00100001, 0b10100101}};
-
-//uint8_t matrixSnow[2][8] ={{0b01000001, 0b00000100, 0b00010000, 0b10000000, 0b00001001, 0b00100000, 0b00000000, 0b10000010},
-//                            {0b10001000, 0b00000000, 0b01000001, 0b00000100, 0b00010000, 0b10000000, 0b00001001, 0b00100000}};
+enum Weather { SUN, CLOUD, RAIN, SNOW };
+enum Weather weather;
+uint8_t patterns[4][2][8] = {MATRIX_SUN, MATRIX_CLOUD, MATRIX_RAIN, MATRIX_SNOW};
 
 static byte state = 0;
 
-//rain
-uint8_t matrixStatus[2][8] ={{0b01000010, 0b01001010, 0b01001010, 0b00001000, 0b00100001, 0b10100101, 0b10100101, 0b10000100},
-                            {0b10001010, 0b00000000, 0b01000010, 0b01001010, 0b01001010, 0b00001000, 0b00100001, 0b10100101}};
-
-//snow
-//uint8_t matrixStatus[2][8] ={{0b01000001, 0b00000100, 0b00010000, 0b10000000, 0b00001001, 0b00100000, 0b00000000, 0b10000010},
-//                            {0b10001000, 0b00000000, 0b01000001, 0b00000100, 0b00010000, 0b10000000, 0b00001001, 0b00100000}};
-
-static byte state = 0;
 void setup() {
   // put your setup code here, to run once:
   for(int i=0; i<MATRIX_SIZE; i++) {
@@ -68,16 +65,15 @@ void setup() {
   pinMode(SCK_PIN, OUTPUT);
   pinMode(RCK_PIN, OUTPUT);
   //MsTimer2::set(800, timerDot); (sun, cloud)1
-
   MsTimer2::set(800, timerDot);
   MsTimer2::start();
-  pinMode(sensor, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  displayMatrix();
+
+  displayMatrix(weather=RAIN);
 }
 
 void clear(){
@@ -92,14 +88,14 @@ void writeRowData(byte data) {
   digitalWrite(RCK_PIN, HIGH);
 }
 
-
-void displayMatrix() {
+void displayMatrix(byte number) {
   int rowbits = 0b10000000;
   for(int row=0; row<MATRIX_SIZE; row++) {
     clear();
     writeRowData(rowbits); // prepare to write the row
-    for(int col=0; col<MATRIX_SIZE; col++)
-      digitalWrite(colPins[7-col], !extract_bits(matrixSun[state][row], 0x01, col));
+    for(int col=0; col<MATRIX_SIZE; col++) {
+      digitalWrite(colPins[7-col], !extract_bits(patterns[number][state][row], 0x01, col));
+    }
     delay(1);
     writeRowData(0);
     rowbits >>= 1; 
